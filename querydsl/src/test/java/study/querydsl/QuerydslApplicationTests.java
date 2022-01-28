c2e0cced9667a94af946bc0cb50289fa816c032b
keundeok2 <keundeok2@gmail.com>
2022-01-28 18:37:27 +0900

    jpa 설정

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -14,6 +16,8 @@ import javax.persistence.PersistenceContext;
 
 @SpringBootTest
 @Transactional
+//@Rollback(false)
+@Commit
 class QuerydslApplicationTests {
 
 //    @Autowired
